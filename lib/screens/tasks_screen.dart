c66ac4afc14c51e3e097294f87811d69ad4228a7
morikann <mrknt0088@gmail.com>
2021-10-02 19:45:31 +0900

    designing todo-items using ListView

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/task_list.dart';
 
 class TasksScreen extends StatelessWidget {
   @override
@@ -50,6 +51,7 @@ class TasksScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
@@ -57,6 +59,7 @@ class TasksScreen extends StatelessWidget {
                   topRight: Radius.circular(20.0),
                 ),
               ),
+              child: TasksList(),
             ),
           ),
         ],
